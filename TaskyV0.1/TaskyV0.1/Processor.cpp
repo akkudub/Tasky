57d349d86a6356fb233d8e1904d2abeebac38365
franklingu <franklingujunchao@gmail.com>
2013-09-25 10:37:43 +0800

    some changes to processor.h and .cpp

--- TaskyV0.1/TaskyV0.1/Processor.cpp
+++ TaskyV0.1/TaskyV0.1/Processor.cpp
@@ -5,6 +5,56 @@ Processor::Processor(){
 	_statusFlag=0;
 }
 
+
+//additional helping methods
+int Processor::breakIntoStringVector(string longStr){
+	stringstream ss(longStr);
+	string tempStr;
+
+	while (std::getline(ss, tempStr, ' ')){
+		_wordsList->push_back(tempStr);
+	}
+
+	return 0;
+}
+
+int Processor::stringToInt(string str){
+	int num;
+	if ((stringstream(str)>>num)){
+		return num;
+	}else{
+		return -1;  //to be defined as const int; negative number will be treated as illegal input
+	}
+}
+
+string Processor::combineStringsWithSpace(int start, int end){
+	string result=_wordsList->at(start);
+	for (int i=start+1;i<end;i++){
+		if(_wordsList->at(i)!=""){
+			result=result+" "+_wordsList->at(i);
+		}
+	}
+	return removeLeadingSpaces(result);
+}
+
+string Processor::combineStringsWithNewLine(int start, int end){
+	string result=taskToString(_tempTaskList.at(start));
+	for (int i=start+1;i<end;i++){
+		result=result+"\n"+taskToString(_tempTaskList.at(i));
+	}
+	return result;
+}
+
+
+string Processor::toLowCaseString(string str){
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+}
+
+string Processor::removeLeadingSpaces(string str){
+	int num=str.find_first_not_of(' ');
+	return str.substr(num);
+}
+
 Processor::~Processor(){
 	delete _wordsList;
 }

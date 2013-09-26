95455408d683536e4c7b3749cfc8de3e39ca87a4
Kaiwen <Kaiwen@localhost>
2013-09-27 01:39:17 +0800

    task updated

--- TaskyV0.1/TaskyV0.1/Task.cpp
+++ TaskyV0.1/TaskyV0.1/Task.cpp
@@ -1,11 +1,16 @@
 #include "Task.h"
 
+string Task::getTitle(){return _title;}
+DateTime Task::getStart(){return *_start;}
+DateTime Task::getEnd(){return *_end;}
+int Task::getType(){return _type;}
+bool& Task::getDone(){return _done;}
+string Task::getComment(){return _comment;}
+
 Task::Task(){
 
 }
 
-bool& Task::getDone(){return _done;}
-	_title=title;
 
 Task::Task(string title, DateTime start, DateTime end, int type, bool done, string comment){
 

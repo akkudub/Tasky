265c957dd4d0f20fd7997f5f36175536936ea719
Huang Kaiwen <huangkaiw3n@users.noreply.github.com>
2013-09-26 17:33:10 +0800

    new functions

--- TaskyV0.1/TaskyV0.1/Task.cpp
+++ TaskyV0.1/TaskyV0.1/Task.cpp
@@ -6,3 +6,35 @@ DateTime Task::getEnd(){return *_end;}
 int Task::getType(){return _type;}
 bool Task::getDone(){return _done;}
 string Task::getComment(){return _comment;}
+
+Task::Task(string title, DateTime start, DateTime end, int type, bool done, string comment){
+
+	_title = title;
+	_start = &start;
+	_end = &end;
+	_type = type;
+	_done = done;
+	_comment = comment;
+
+}
+
+bool Task::isEqualTo(Task& compare){
+
+	return compare.getTitle() == _title
+		&& compare.getStart() == *_start
+		&& compare.getEnd() == *_end;
+
+}
+
+
+//clash: if start >= _start && < _end || if end > _start && <= _end
+bool Task::isClashingWith(Task& compare){
+
+	bool startClash = compare.getStart() >= *_start
+		&& compare.getStart() < *_end;
+	bool endClash = compare.getEnd() > *_start
+		&& compare.getEnd() <= *_end;
+
+	return startClash || endClash;
+}
+

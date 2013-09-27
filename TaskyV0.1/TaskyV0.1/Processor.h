92f79631d521aea2c06681f7370cce3cdcf4cf73
Akshat Dubey <akshatdubey@nus.edu.sg>
2013-09-27 12:53:38 +0800

    finished update, remove, mark methods. there are issues, please read comments in update method(line 180)

--- TaskyV0.1/TaskyV0.1/Processor.h
+++ TaskyV0.1/TaskyV0.1/Processor.h
@@ -2,17 +2,17 @@
 #define _Processor_H_
 
 /*
- *This class is the central controlling unit. It will take string from UI and process it, acknowledge UI
- *if input format is not suitable or it will go ahead and extract command out of input string and call methods
- *in Logic/history/FileProcessing to carry out the command.
- *
- *Main role: string processing, central logic unit, front wall of logic.
- *
- *Additional notice: this class will take the pointer of the _itemList in Logic class to ease the cost of
- *displaying. But the author of this class will only read from _itemList. This method is more like data-binding
- *with only programmers' protection.
- *
- *Author: Akshat, Junchao
+*This class is the central controlling unit. It will take string from UI and process it, acknowledge UI
+*if input format is not suitable or it will go ahead and extract command out of input string and call methods
+*in Logic/history/FileProcessing to carry out the command.
+*
+*Main role: string processing, central logic unit, front wall of logic.
+*
+*Additional notice: this class will take the pointer of the _itemList in Logic class to ease the cost of
+*displaying. But the author of this class will only read from _itemList. This method is more like data-binding
+*with only programmers' protection.
+*
+*Author: Akshat, Junchao
 */
 
 #include <sstream>
@@ -31,17 +31,17 @@ class Processor{
 private:
 	vector<Task> _tempTaskList; //hold the tasks returned by search and display
 	vector<string> _stringList; //hold the tasks in a string format, pass to FileProcessing and that
-	                            //will read and write accordingly
+	//will read and write accordingly
 	vector<Task>* _taskListPointer;
 	LogicStub _logic;
 	History _history;
 	FileProcessing _fileProcessing;
 	vector<string> *_wordsList;
 	int _statusFlag;  //0 : last operation has ended;
-	                  //1 : update operation has not ended;
-	                  //2 : remove operation has not ended;
-	                  //3 : mark operation has not ended;
-	                  
+	//1 : update operation has not ended;
+	//2 : remove operation has not ended;
+	//3 : mark operation has not ended;
+
 private:
 	static const string EMPTY_STRING;
 	static const string NEW_LINE_STRING;
@@ -58,6 +58,9 @@ private:
 	static const string WRONG_INPUT;
 	static const string UNEXPECTED_ERROR;
 	static const string UPDATE_CHOOSE_TASK;
+	static const string UPDATE_MESSAGE_WARNING_SAME;
+	static const string UPDATE_MESSAGE_WARNING_CLASH;
+	static const string UPDATE_MESSAGE_FAILURE;
 	static const string REMOVE_CHOOSE_TASK;
 	static const string MARK_CHOOSE_TASK;
 	static const string NO_SUCH_TASK;
@@ -71,23 +74,23 @@ private:
 	static const char ZERO;
 	static const char NINE;
 	static const char SPACE;
-	
+
 public:
 	Processor();
 	string mainProcessor(string); //processes the string, calls the appropriate logic methods, returns string
-	                              //to UI
-	                              //create a new vector of strings and delete that at the end
+	//to UI
+	//create a new vector of strings and delete that at the end
 	~Processor();
 
 private:
 	//level 1 abstraction
 	string addCommandProcessor();
 	string displayCommandProcessor();
-	
+
 	string updateCommandProcessor();  //change the flag if necessary
 	string removeCommandProcessor();  //change the flag if necessary
 	string markCommandProcessor();  //change the flag if necessary
-	
+
 	string otherCommandProcessor();
 
 	//for add command processing

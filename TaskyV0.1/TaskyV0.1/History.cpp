c4aa50b2af7d23422335178e3853672b618c3769
Joan <Joan@localhost>
2013-10-02 10:05:32 +0800

    Added documentation

--- TaskyV0.1/TaskyV0.1/History.cpp
+++ TaskyV0.1/TaskyV0.1/History.cpp
@@ -1,9 +1,22 @@
 #include "History.h"
 
+/*Purpose:
+* Clears the vector containing history actions upon start up
+*/
 History::History() {
 	_historyRecord.clear();
 }
 
+/*Purpose:
+* Retrieves the last action from vector of history records
+*
+* Param:
+* hist - last action done will be passed back to calling function
+* via this referenced string
+*
+* Returns:
+* Success in retrival; Empty records, nothing to be undone; Failure
+*/
 int History::undo(string& hist) {
 	int size = _historyRecord.size();
 	if(!_historyRecord.empty()) {
@@ -18,6 +31,15 @@ int History::undo(string& hist) {
 	return UNDO_FAILURE;
 }
 
+/*Purpose:
+* saves the successful action into vectory of history records
+*
+* Param:
+* hist - string containing successful user action preformed
+*
+* Returns:
+* Success in putting the record into history; Failure
+*/
 int History::record(string hist) {
 	int size = _historyRecord.size();
 	_historyRecord.push_back(hist);

36b2462fa901bbb8bb9d0393360d5de6772e2ae3
Franklingu <franklingujunchao@gmail.com>
2013-10-04 18:57:42 +0800

    some overloaded functions and first xml file generated--no in the commit after clean solution

--- TaskyV0.1/TaskyV0.1/BasicDateTime.cpp
+++ TaskyV0.1/TaskyV0.1/BasicDateTime.cpp
@@ -1,28 +1,69 @@
 #include "BasicDateTime.h"
 
-BasicDateTime::BasicDateTime()
-{
-}
-
-BasicDateTime::BasicDateTime(int year, int month, int day, int hour, int minute)
-{
-	_year = year;
-	_month = month;
-	_day = day;
-	_hour = hour;
-	_minute = minute;
+BasicDateTime::BasicDateTime(){
+	_year=0;
+	_month=0;
+	_day=0;
+	_hour=0;
+	_minute=0;
+	_sec=0;
 }
 
 BasicDateTime::~BasicDateTime(){
 }
 
-void BasicDateTime::setDateTime(int year, int month, int day, int hour, int minute)
-{
+BasicDateTime::BasicDateTime(int year, int month, int day){
+	_year=year;
+	_month=month;
+	_day=day;
+	_hour=23;
+	_minute=59;
+	_sec=59;
+}
+
+BasicDateTime::BasicDateTime(int year, int month, int day, int hour, int minute){
 	_year = year;
 	_month = month;
 	_day = day;
 	_hour = hour;
 	_minute = minute;
+	_sec=0;
+}
+
+BasicDateTime::BasicDateTime(int year, int month, int day, int hour, int minute, int sec){
+	_year = year;
+	_month = month;
+	_day = day;
+	_hour = hour;
+	_minute = minute;
+	_sec=sec;
+}
+
+void BasicDateTime::setDateTime(int year, int month, int day){
+	_year = year;
+	_month = month;
+	_day = day;
+	_hour = 23;
+	_minute = 59;
+	_sec=59;
+}
+
+void BasicDateTime::setDateTime(int year, int month, int day, int hour, int minute){
+	_year = year;
+	_month = month;
+	_day = day;
+	_hour = hour;
+	_minute = minute;
+	_sec=0;
+}
+
+void BasicDateTime::setDateTime(int year, int month, int day, int hour, int minute, int sec){
+	_year = year;
+	_month = month;
+	_day = day;
+	_hour = hour;
+	_minute = minute;
+	_sec=sec;
 }
 
 int BasicDateTime::getYear(){ 
@@ -48,7 +89,7 @@ int BasicDateTime::getSec(){
 	return _sec;
 }
 
-string BasicDateTime::getDateTime(){
+string BasicDateTime::getDateTimeString(){
 	DateTime temp = DateTime(_year, _month, _day,  _hour, _minute, _sec);
 	return marshal_as<std::string>(temp.ToString()); 
 }

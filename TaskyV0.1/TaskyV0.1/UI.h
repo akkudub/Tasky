6f00b75257caf489265608711f038e4fc0236e92
Joan <Joan@localhost>
2013-10-10 21:35:46 +0800

    Changed commenting style

--- TaskyV0.1/TaskyV0.1/UI.h
+++ TaskyV0.1/TaskyV0.1/UI.h
@@ -17,36 +17,50 @@ class UI
 {
 private:
 	Processor _processor;
-	///Purpose:
-	///Displays a welcome message to the user
-	///Displays the tasks of the day/pending tasks
+	/**
+	* Purpose:
+	* Displays a welcome message to the user
+	* Displays the tasks of the day/pending tasks
+	*/
 	void displayWelcomeMessage();	//should display welcome messages + task of the day
-	///Purpose:
-	///Takes in user command & put into string command
-	///@param command - stores the user input 
+	/**
+	* Purpose:
+	* Takes in user command & put into string command
+	* @param command - stores the user input 
+	*/
 	void displayCommandMessage(string& command);
-	///Purpose:
-	/// Send the user's command to the Processor
-	///@param command - user input to be passed to Processor
+	/**
+	* Purpose:
+	* Send the user's command to the Processor
+	*@param command - user input to be passed to Processor
+	*/
 	void displayProcessorMessage(string command,HANDLE hConsole);
-	///Purpose:
-	///Display Closing Message before exiting the program
-	///Possible to save the tasks to the Log before exiting
+	/**
+	* Purpose:
+	* Display Closing Message before exiting the program
+	* Possible to save the tasks to the Log before exiting
+	*/
 	void displayExitMessage();
-	///Purpose:
-	///Checks if the user wishes to exit
-	///@param command - stores the user input 
-	///@return True- Exit, False - Do Not Exit
+	/** 
+	* Purpose:
+	* Checks if the user wishes to exit
+	* @param command - stores the user input 
+	* @return True- Exit, False - Do Not Exit
+	*/
 	bool shouldExit(string command);
-	///Purpose:
-	///Converts the user command to lower casing
-	///@param str - string to be converted to lower case
+	/**
+	* Purpose:
+	* Converts the user command to lower casing
+	*@param str - string to be converted to lower case
+	*/
 	void convertLowerCase(string& str);
 public:
-	///Purpose:
-	///Function called when program is started up
-	///Takes in user input/commands and passes to Processor
-	///Checks if user wishes to exit the program
+	/**
+	* Purpose:
+	* Function called when program is started up
+	* Takes in user input/commands and passes to Processor
+	*Checks if user wishes to exit the program
+	*/
 	void UI_interface(); //takes in user input
 };
 

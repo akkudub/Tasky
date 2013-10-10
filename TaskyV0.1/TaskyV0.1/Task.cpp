332bd8d7b0b4bdab91341ad1b41c3abd5b2cc19e
Akshat Dubey <akkudub@users.noreply.github.com>
2013-10-10 21:18:31 +0800

    updated Processor to make use of Interpreter class, added setType in Task

---
+++ TaskyV0.1/TaskyV0.1/Task.cpp
@@ -0,0 +1,93 @@
+#include "Task.h"
+
+string Task::getTitle(){
+	return _title;
+}
+
+BasicDateTime Task::getStart(){
+	return _start;
+}
+
+BasicDateTime Task::getEnd(){
+	return _end;
+}
+
+int Task::getType(){
+	return _type;
+}
+
+bool Task::getDone(){
+	return _done;
+}
+
+string Task::getComment(){
+	return _comment;
+}
+
+void Task::toggleDone(){
+	_done=!_done;
+}
+
+Task::Task(){
+
+}
+
+
+Task::Task(string title, BasicDateTime start, BasicDateTime end, int type, bool done, string comment){
+
+	_title = title;
+	_start = start;
+	_end = end;
+	_type = type;
+	_done = done;
+	_comment = comment;
+
+}
+
+Task::~Task()
+{
+	
+}
+
+bool Task::isEqualTo(Task another){
+
+	return another.getTitle() == _title
+		&& !another.getStart().compareTo(_start)
+		&& !another.getEnd().compareTo(_end);
+
+}
+
+
+bool Task::isClashingWith(Task another){
+	bool startClash=false;
+	bool endClash=false;
+
+	if(another.getStart().compareTo(_start) >= 0 && another.getStart().compareTo(_end) < 0)
+		startClash = true;
+	else
+		startClash = false;
+
+	if(another.getEnd().compareTo(_start) > 0 && another.getEnd().compareTo(_end) <= 0)
+		endClash = true;
+	else
+		endClash = false;
+
+	return startClash || endClash;
+
+}
+
+void Task::setTitle(string title){
+	_title = title;
+}
+	
+void Task::setStartDate(BasicDateTime start){
+	_start = start;
+}
+	
+void Task::setEndDate(BasicDateTime end){
+	_end = end;
+}
+
+void Task::setType(int type){
+	_type = type;
+}

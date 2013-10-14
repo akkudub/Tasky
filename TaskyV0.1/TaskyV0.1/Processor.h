1a0a05dce9c6c04eb1e7e806b1fc3a00d1b2da18
Akshat Dubey <akshatdubey@nus.edu.sg>
2013-10-14 18:17:58 +0800

    made the error codes and error messages in processor

--- TaskyV0.1/TaskyV0.1/Processor.h
+++ TaskyV0.1/TaskyV0.1/Processor.h
@@ -4,11 +4,11 @@
 /*
 *This class is the central controlling unit. It will take string from UI and process it, acknowledge UI
 *if input format is not suitable or it will go ahead and extract command out of input string and call methods
-*in Logic/history/FileProcessing to carry out the command.
+*in TaskList/history/FileProcessing to carry out the command.
 *
 *Main role: string processing, central logic unit, front wall of logic.
 *
-*Additional notice: this class will take the pointer of the _itemList in Logic class to ease the cost of
+*Additional notice: this class will take the pointer of the _itemList in TaskList class to ease the cost of
 *displaying. But the author of this class will only read from _itemList. This method is more like data-binding
 *with only programmers' protection.
 *
@@ -23,7 +23,7 @@
 #include "BasicDateTime.h"
 #include "History.h"
 #include "FileProcessing.h"
-#include "Logic.h"
+#include "TaskList.h"
 #include "Interpreter.h"
 #include "Essential.h"
 
@@ -32,17 +32,17 @@ using namespace msclr::interop;
 
 class Processor{
 	/*
-	*_tempTaskList: hold the tasks returned by logic.search and logic.display
+	*_tempTaskList: hold the tasks returned by _taskList.search and _taskList.display
 	*_stringList: hold tasks in string format, used to pass to fileProcessing
 	*            for it to perform read/write file
-	*_taskListPointer: a pointer pointing to logic.tasklist. will only read
-	*_logic: an instance of Logic
+	*_taskListPointer: a pointer pointing to _taskList.tasklist. will only read
+	*_taskList: an instance of TaskList
 	*_history: an instance of History
 	*_fileProcessing: an instance of FileProcessing
 	*_wordList: a temporary way to help to parse command, will be changed later
 	*_statusFlag: 0--last operation ended properly;
 	*             1--remove operation has not ended;
-	*             2--update operation has not ended;
+	*             2--rename operation has not ended;
 	*             3--reschedule operation has not ended;
 	*			  4--mark operation has not ended;
 	*/
@@ -56,7 +56,7 @@ private:
 
 	vector<string> _stringList; 
 	vector<Task>* _taskListPointer;
-	Logic _logic;
+	TaskList _taskList;
 	History _history;
 	Interpreter _interpreter;
 	FileProcessing _fileProcessing;
@@ -70,23 +70,54 @@ private:
 	static const string TO_KEY_WORD;
 	static const string BY_KEY_WORD;
 
-	static const string ADD_TASK_SUCCESS;
-	static const string ADD_TASK_FAILURE_DUPLICATE;
-	static const string ADD_TASK_FAILURE_UNEXPECTED;
-	static const string ADD_TASK_WARNING_CLASH;
-	static const string DISPLAY_TASK_SUCCESS;
-	static const string DISPLAY_TASK_FAILURE_UNEXPECTED;
-	static const string WRONG_INPUT;
-	static const string UNEXPECTED_ERROR;
-	static const string UPDATE_CHOOSE_TASK;
-	static const string UPDATE_MESSAGE_WARNING_SAME;
-	static const string UPDATE_MESSAGE_WARNING_CLASH;
-	static const string UPDATE_MESSAGE_FAILURE;
-	static const string REMOVE_CHOOSE_TASK;
-	static const string MARK_CHOOSE_TASK;
-	static const string NO_SUCH_TASK;
-	static const string FILE_SAVE_SUCCESS;
-	static const string FILE_SAVE_FAILURE;
+	static const int MAX_MESSAGES = 50;
+	static const string MESSAGE_ARRAY [MAX_MESSAGES];
+
+	static const string MESSAGE_OVERALL_SUCCESS;
+	static const string MESSAGE_OVERALL_WARNING;
+	static const string MESSAGE_OVERALL_ERROR;
+	static const string MESSAGE_OVERALL_PROMPT;
+	static const string MESSAGE_OVERALL_EXIT;
+
+	static const string MESSAGE_SUCCESS_ADD;
+	static const string MESSAGE_SUCCESS_REMOVE;
+	static const string MESSAGE_SUCCESS_DISPLAY;
+	static const string MESSAGE_SUCCESS_RENAME;
+	static const string MESSAGE_SUCCESS_RESCHEDULE;
+	static const string MESSAGE_SUCCESS_MARK;
+	static const string MESSAGE_SUCCESS_SEARCH;
+	static const string MESSAGE_SUCCESS_UNDO;
+	static const string MESSAGE_SUCCESS_LOAD;
+	static const string MESSAGE_SUCCESS_SAVE;
+	static const string MESSAGE_SUCCESS_RECORD;
+	static const string MESSAGE_SUCCESS_REDO;
+
+	static const string MESSAGE_WARNING_ADD_CLASH;
+	static const string MESSAGE_WARNING_ADD_DUPLICATE;
+	static const string MESSAGE_WARNING_DISPLAY_NO_RESULT;
+	static const string MESSAGE_WARNING_UPDATE_SAME;
+	static const string MESSAGE_WARNING_UPDATE_CLASH;
+	static const string MESSAGE_WARNING_MARK_NO_CHANGE;
+	static const string MESSAGE_WARNING_SEARCH_NO_RESULT;
+	static const string MESSAGE_WARNING_UNDO_NO_TASKS;
+	static const string MESSAGE_WARNING_WRONG_INPUT;
+
+	static const string MESSAGE_ERROR_ADD;
+	static const string MESSAGE_ERROR_REMOVE;
+	static const string MESSAGE_ERROR_DISPLAY;
+	static const string MESSAGE_ERROR_RENAME;
+	static const string MESSAGE_ERROR_RESCHEDULE;
+	static const string MESSAGE_ERROR_MARK;
+	static const string MESSAGE_ERROR_SEARCH;
+	static const string MESSAGE_ERROR_UNDO;
+	static const string MESSAGE_ERROR_RECORD;
+	static const string MESSAGE_ERROR_LOAD_OPENFILE;
+	static const string MESSAGE_ERROR_SAVE_SAVEFILE;
+
+	static const string MESSAGE_PROMPT_REMOVE_CHOOSE;
+	static const string MESSAGE_PROMPT_RENAME_CHOOSE;
+	static const string MESSAGE_PROMPT_RESCHEDULE_CHOOSE;
+	static const string MESSAGE_PROMPT_MARK_CHOOSE;
 
 	static const char NEW_LINE;
 	static const char SLASH;
@@ -137,7 +168,7 @@ private:
 	string printTimedTask(Task);
 	string printStatus(bool);
 	string removeLeadingSpaces(string);
-	string taskVecToStringVec(vector<string>& stringTasks);
+	vector<string> taskVecToStringVec(vector<Task> taskList);
 };
 
 #endif

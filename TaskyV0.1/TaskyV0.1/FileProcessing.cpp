16360833e20b9aa080a464f3a39b884364794f4c
Joan <Joan@localhost>
2013-10-16 13:32:11 +0800

    redoFunction implemented

---
+++ TaskyV0.1/TaskyV0.1/FileProcessing.cpp
@@ -0,0 +1,67 @@
+#include "FileProcessing.h"
+
+
+int FileProcessing::load(vector<string>& data){
+	try{
+		ifstream input(_LOGNAME);
+		if(!input.is_open()) {
+			return STATUS_CODE_SET_ERROR::ERROR_LOAD_OPENFILE;
+		} else {
+			if (input.good()) {
+				if (!emptyFile()) {
+					data.clear();
+					while(input) {
+						string line;
+						getline(input, line);
+						data.push_back(line);
+					}
+				} else {
+					input.close();
+					return STATUS_CODE_SET_WARNING::WARNING_LOAD_EMPTY_FILE;	
+				}
+			} else {
+				return STATUS_CODE_SET_ERROR::ERROR_LOAD_OPENFILE;
+			}
+		}
+		input.close();
+		return STATUS_CODE_SET_SUCCESS::SUCCESS_LOAD;
+	}catch(exception e) {
+		return STATUS_CODE_SET_ERROR::ERROR_LOAD_OPENFILE;
+	}
+}
+
+
+int FileProcessing::save(vector<string>& data){
+	try{
+		ofstream output(_LOGNAME);
+		//check if file exist
+		if (!output.is_open()) {
+			output.clear();
+			output.open(_LOGNAME, ofstream::out|ofstream::trunc);
+		}
+		//check if file is created, ready for writing
+		if (output.good()) {
+			for(string s:data) {
+				output << s << endl;
+			}
+		} else {
+			output.close();
+			return STATUS_CODE_SET_ERROR::ERROR_SAVE_SAVEFILE;
+		}
+		output.close();
+		return STATUS_CODE_SET_SUCCESS::SUCCESS_LOAD;
+	}catch(exception e){
+		return STATUS_CODE_SET_ERROR::ERROR_SAVE_SAVEFILE;
+	}
+}
+
+
+bool FileProcessing::emptyFile() {
+	bool isEmpty = false;
+	ifstream inf;
+	inf.open(_LOGNAME);
+	if(inf.peek() == ifstream::traits_type::eof())
+		isEmpty = true;
+	inf.close();
+	return isEmpty;
+}

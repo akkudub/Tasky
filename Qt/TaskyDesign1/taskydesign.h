ea3654942cb045cbe7db2eba291cac9bccb2a874
Huang Kaiwen <huangkaiw3n@users.noreply.github.com>
2013-11-06 20:53:42 +0800

    Fixed bug with search tasks

---
+++ Qt/TaskyDesign1/taskydesign.h
@@ -0,0 +1,54 @@
+#ifndef TASKYDESIGN1_H
+#define TASKYDESIGN1_H
+
+#include <QtWidgets/QMainWindow>
+#include <QListWidget>
+#include <vector>
+#include "ui_taskydesign.h"
+#include "Processor.h"
+
+class TaskyDesign : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	TaskyDesign(QWidget *parent = 0);
+	~TaskyDesign();
+
+private:
+	Processor* _logic;
+	std::string _msg;
+	std::vector<std::string> _vec;
+
+private:
+	static const QString STYLE_SHEET_DISPLAY_PANEL;
+	static const QString STYLE_SHEET_WINDOW;
+	static const QString STYLE_SHEET_INPUT_BOX;
+
+	static const QString KEYWORD_HELP;
+	static const QString KEYWORD_ABOUT;
+	static const QString KEYWORD_EXIT;
+	static const QString KEYWORD_MINIMIZE;
+
+	static const QString ICON_STRING;
+
+private slots:
+	void processInputString();
+	void showFullTextOfSelected();
+
+private:
+	void changeUIStyle();
+	void help();
+	void about();
+	void minimizeWindow();
+	void exit();
+	bool equalsToKeywordWithoutCase(const QString& input, const QString& keyword);
+	void sendStdStringToBackEnd(QString input);
+	std::string preserveFirstString(int num);
+	std::string fullString(int num);
+
+private:
+	Ui::TaskyDesign1Class ui;
+};
+
+#endif // TASKYDESIGN1_H

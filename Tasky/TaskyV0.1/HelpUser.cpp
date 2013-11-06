b548e299b6fb4a429d09c6d94e2a07523db5cbe3
franklingu <franklingujunchao@gmail.com>
2013-11-06 21:52:49 +0800

    changed some format of the helpuser

--- Tasky/TaskyV0.1/HelpUser.cpp
+++ Tasky/TaskyV0.1/HelpUser.cpp
@@ -5,26 +5,27 @@ const string HelpUser::HELP_GENERAL_MSG      = "Welcome to the help panel\n"
 											   "type in the command following the right format and all the rest will\n"
 											   "will be taken care of by the software. Have fun using it!\n"
 											   "For additional help on a specific topic, you can try\n"
-											   "help -add			for the help regarding to add operation\n"
-											   "help -remove		for the help regarding to remove operation\n"
-											   "help -search		for the help regarding to search operation\n"
-											   "help -mark   		for the help regarding to mark operation\n"
-											   "help -reschedule	for the help regarding to reschedule operation\n"
-											   "help -rename		for the help regarding to rename operation\n"
-											   "help -display		for the help regarding to display operation\n"
-											   "help -title			for the help regarding to title\n"
-											   "help -date			for the help regarding to date\n"
-											   "help -time			for the help regarding to time\n"
-											   "help -comment		for the help regarding to comment\n"
-											   "help -others		for the help regarding to others\n"
-											   "help -about			for the help regarding to about\n\n";
+											   "help -add           for the help regarding to add operation\n"
+											   "help -remove        for the help regarding to remove operation\n"
+											   "help -search        for the help regarding to search operation\n"
+											   "help -mark          for the help regarding to mark operation\n"
+											   "help -reschedule    for the help regarding to reschedule operation\n"
+											   "help -rename        for the help regarding to rename operation\n"
+											   "help -display       for the help regarding to display operation\n"
+											   "help -title         for the help regarding to title\n"
+											   "help -date          for the help regarding to date\n"
+											   "help -time          for the help regarding to time\n"
+											   "help -comment       for the help regarding to comment\n"
+											   "help -others        for the help regarding to others\n"
+											   "help -about         for the help regarding to about\n\n";
 const string HelpUser::HELP_ADD_MSG          = "To add a task, enter one of the followings:\n"
 	                                           "Format1: add '[Title]' from [StartDateTime] to [EndDateTime] -m [Comment]\n"
 											   "Format2: add '[Title]' by [Deadline] -m [Comment]\n"
-											   "Format3: add '[Title]' -m [Comment]\n"
+											   "Format3: add '[Title]' -m [Comment]\n\n"
 											   "Note1: The title must be enclosed by two single quotes. Use help -title for more help\n"
 											   "Note2: The various formats of date and time are explained in help -date and help -time\n"
-											   "Note3: The comment is optional. Leave it blank if not applicable for you. help -comment for more help.\n";
+											   "Note3: The comment is optional. Leave it blank if not applicable for you.\n"
+											   "    Type help -comment for more help.\n";
 const string HelpUser::HELP_REMOVE_MSG       = "To remove a task, enter the following:\n"
 	                                           "Format: remove '[Title]'\n"
 											   "If prompted for selection of task to remove, enter the number only."
@@ -48,16 +49,20 @@ const string HelpUser::HELP_RENAME_MSG       = "To rename a task, enter the foll
 const string HelpUser::HELP_DISPLAY_MSG      = "To display all tasks: display all\n"
 											   "To display pending tasks: display pending\n"
 											   "To display today's task: display today";
-const string HelpUser::HELP_TITLE_MSG        = "";
+const string HelpUser::HELP_TITLE_MSG        = "Enclose the title with single quotes. Any single quotes inside title will not\n" 
+	                                           "    affect the function of title\n"
+											   "Format: '[title]'\n"
+											   "Example: 'it's a title\n'";
 const string HelpUser::HELP_DATE_MSG         = "The format of date is as followings:\n"
 	                                           "Format1: DD/MM/YYYY\n"
 											   "Format2: DD/MM/YY\n"
 											   "Format3: today/tdy, tomorrow/tmr\n"
 											   "Format4: this/next monday/tuesday/wednesday/thursday/friday/saturday/sunday\n"
-											   "Format5: this/next mon/tue/wed/thu/fri/sat/sun\n"
+											   "Format5: this/next mon/tue/wed/thu/fri/sat/sun\n\n"
 											   "Note1: Format1 e.g.: 12/13/2012\n"
 											   "Note2: Format2 e.g.: 12/13/13\n"
-											   "Note3: Format3, input either today or its shortcut spelling tdy for today. Same goes for tomorrow\n"
+											   "Note3: Format3, input either today or its shortcut spelling tdy for today.\n"
+											   "    Same goes for tomorrow\n"
 											   "Note4: Format4, input one of 7 days in a week\n"
 											   "Note5: Format5, the shortcut spellings for Format4\n"
 											   "Additional notes: currently the range of date is between year 1900 and year 2300\n";
@@ -67,9 +72,9 @@ const string HelpUser::HELP_COMMENT_MSG      = "To add a comment behind a task, 
 											   "For example: add 'Shopping For XMas Gifts' by 25/12/2013 -m bring recepient list\n";
 const string HelpUser::HELP_OTHERS_MSG       = "For more information, please refer to Tasky User Manual.";
 const string HelpUser::HELP_ABOUT_MSG        = "Developed by AY2013 Semester1 CS2103 F09-2C Team.\n"
-	                                           "Come and visit the  project at : http://code.google.com/p/cs2103aug2013-f09-2c/";
+	                                           "Come and visit the project at : http://code.google.com/p/cs2103aug2013-f09-2c/";
 
-const string HelpUser::GENERAL_STR           = " ";
+const string HelpUser::GENERAL_STR           = "";
 const string HelpUser::ADD_STR               = " -add";
 const string HelpUser::REMOVE_STR            = " -remove";
 const string HelpUser::SEARCH_STR            = " -search";

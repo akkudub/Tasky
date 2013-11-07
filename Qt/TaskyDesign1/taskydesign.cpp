dce02aff4a733f09a25c08d5d70c6806f7ee0879
franklingu <franklingujunchao@gmail.com>
2013-11-08 00:50:52 +0800

    ...the Qt thing is quite messy

--- Qt/TaskyDesign1/taskydesign.cpp
+++ Qt/TaskyDesign1/taskydesign.cpp
@@ -1,38 +1,33 @@
-#include <qmessagebox.h>
 #include "taskydesign.h"
-#include <qdebug.h>
+#include <QCompleter>
 
 const QString TaskyDesign::STYLE_SHEET_WINDOW = "background-color: #CCFF99; border-radius: 10px;";
-const QString TaskyDesign::STYLE_SHEET_DISPLAY_PANEL = "QListWidget#DisplayPanel{background-color: #F1FFFF; border-radius: 5px;}\n"
-	                                                   "QListView#DisplayPanel::item {margin-bottom: 5px; border-radius: 5px;}\n"
-	                                                   "QListView#DisplayPanel::item:selected {background-color: #5CE6E6;}";
+const QString TaskyDesign::STYLE_SHEET_DISPLAY_PANEL = "QTextEdit#DisplayPanel{background-color: #ECFFF9; border-radius: 7px; margin-bottom: 20px;}";
 const QString TaskyDesign::STYLE_SHEET_INPUT_BOX = "QLineEdit#InputBox{background-color: #FFFFFF;}";
 
-const QString TaskyDesign::KEYWORD_ABOUT = "about";
 const QString TaskyDesign::KEYWORD_EXIT = "exit";
-const QString TaskyDesign::KEYWORD_HELP = "help";
 const QString TaskyDesign::KEYWORD_MINIMIZE = "minimize";
 
 const QString TaskyDesign::ICON_STRING = "tasky_icon.ico";
 
 TaskyDesign::TaskyDesign(QWidget *parent): QMainWindow(parent){
+	wordlist<<"add ''"<<"from"<<"to"<<"delete"<<"display";
 	ui.setupUi(this);
 	_logic=new Processor();
+	QCompleter *completer=new QCompleter(wordlist, this->ui.InputBox);
+	ui.InputBox->setCompleter(completer);
+
 
 	changeUIStyle();
 
+	connect(ui.InputBox, SIGNAL(textChanged(const QString& text)), this, SLOT(updateStatusBar()));
 	connect(ui.InputBox, SIGNAL(returnPressed()), this, SLOT(processInputString()));
-	connect(ui.DisplayPanel, SIGNAL(itemSelectionChanged()), this, SLOT(showFullTextOfSelected()));
 }
 
 void TaskyDesign::processInputString(){
 	QString input=ui.InputBox->text();
 	ui.InputBox->clear();
-	if (equalsToKeywordWithoutCase(input, KEYWORD_HELP)){
-		help();
-	}else if(equalsToKeywordWithoutCase(input, KEYWORD_ABOUT)){
-		about();
-	}else if(equalsToKeywordWithoutCase(input, KEYWORD_MINIMIZE)){
+	if(equalsToKeywordWithoutCase(input, KEYWORD_MINIMIZE)){
 		minimizeWindow();
     }else if(equalsToKeywordWithoutCase(input, KEYWORD_EXIT)){
         exit();
@@ -41,17 +36,10 @@ void TaskyDesign::processInputString(){
 	}
 }
 
-void TaskyDesign::showFullTextOfSelected(){
-	int size=_vec.size();
-	//int size=ui.DisplayPanel->count();
-	for (int i=0;i<size;i++){
-		if (i>=ui.DisplayPanel->count()){
-			continue;
-		}else if (ui.DisplayPanel->item(i)->isSelected()){
-			ui.DisplayPanel->item(i)->setText(QString::fromStdString(fullString(i)));
-		}else{
-			ui.DisplayPanel->item(i)->setText(QString::fromStdString(preserveFirstString(i)));
-	    }
+void TaskyDesign::updateStatusBar(){
+	QString input=ui.InputBox->text();
+	if (input=="add" || input=="ad" || input=="a"){
+		ui.statusBar->showMessage("add operation: you can type help -add for more information.");
 	}
 }
 
@@ -61,18 +49,6 @@ void TaskyDesign::changeUIStyle(){
 	this->setWindowIcon(QIcon(ICON_STRING));
 	ui.DisplayPanel->setStyleSheet(STYLE_SHEET_DISPLAY_PANEL);
 	ui.InputBox->setStyleSheet(STYLE_SHEET_INPUT_BOX);
-}
-
-void TaskyDesign::help(){
-	QMessageBox help;
-	help.setText("help here");
-	help.exec();
-}
-
-void TaskyDesign::about(){
-	QMessageBox about;
-	about.setText("about us");
-	about.exec();
 }
 
 void TaskyDesign::minimizeWindow(){
@@ -89,26 +65,29 @@ bool TaskyDesign::equalsToKeywordWithoutCase(const QString& input, const QString
 }
 
 void TaskyDesign::sendStdStringToBackEnd(QString input){
-	_logic->UImainProcessor(input.toStdString(), _msg, _vec);
+	_msgType=_logic->UImainProcessor(input.toStdString(), _msg, _vec);
 	ui.DisplayPanel->clear();
 	ui.statusBar->clearMessage();
 	ui.statusBar->showMessage(QString::fromStdString(_msg), 0);
 	int size=_vec.size();
+	ui.DisplayPanel->setHtml(combineOutput(size));
+}
+
+QString TaskyDesign::formatString(int num){
+	QString iniStr="";
+	std::string str=_vec.at(num);
+	int pos1=0;
+	int pos2=str.find('\n');
+	iniStr.append("<font color='DeepPink'>" + QString::fromStdString(str.substr(pos1,pos2)) + "<br/></font>");
+	return iniStr;
+}
+
+QString TaskyDesign::combineOutput( int size ){
+	QString initStr="";
 	for (unsigned int i=0;i<size;i++){
-		ui.DisplayPanel->addItem(QString::fromStdString(preserveFirstString(i)));
+		initStr.append(formatString(i));
 	}
-	if (ui.DisplayPanel->count()>=1){
-		ui.DisplayPanel->item(0)->setSelected(true);
-	}
-}
-
-std::string TaskyDesign::preserveFirstString(int num){
-	std::string str= _vec.at(num);
-	return str.substr(0, str.find_first_of('\n'));
-}
-
-std::string TaskyDesign::fullString(int num){
-	return _vec.at(num);
+	return initStr;
 }
 
 TaskyDesign::~TaskyDesign(){
